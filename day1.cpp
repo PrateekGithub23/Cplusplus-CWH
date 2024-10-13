6cc2bd78d70d82e0b42f9c69ba32ef1b9c9b79e1
PrateekGithub23 <ytprateek23@gmail.com>
2024-10-13 10:14:35 -0500

    Made some changes

--- day1.cpp
+++ day1.cpp
@@ -1,8 +1,10 @@
-// hello
 #include <iostream>
-using namespace std;
 
-int main(){
-    cout<<"Hello World!";
+int main() {
+    // Loop from 1 to 5
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "Iteration: " << i << std::endl;
+    }
+
     return 0;
 }
