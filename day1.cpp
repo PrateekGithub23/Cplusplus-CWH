37c674ae5b12bef507d290976339f32f63f7364a
PrateekGithub23 <ytprateek23@gmail.com>
2024-10-18 12:21:51 -0500

    learnt about conditionals/loops - small examples

--- day1.cpp
+++ day1.cpp
@@ -1,10 +1,59 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    // Loop from 1 to 5
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "Iteration: " << i << std::endl;
-    }
 
-    return 0;
+// --------------------------------------------------------------------------
+
+    // If and Else Statements (incl. else if)
+
+            /*
+            int age;
+            cout<<"tell ur age: "<<endl;
+            cin>>age;
+
+            if (age<18){
+                cout<<"You are not allowed to drive ";
+            }
+            else if(age=18){
+                cout<<"Get your license....meet the requirements"<<endl;
+            }
+            else{
+                cout<<"You are allowed to drive.....drive save";
+
+            } */
+
+
+// --------------------------------------------------------------------------
+
+    // For Loop
+
+            /* int a = 6;
+            for (a;a<=7;a++){
+                cout<<a*2<<endl;
+            } */
+
+// --------------------------------------------------------------------------
+    // While loop
+
+            /* int i = 5;
+            while (i < 10)
+            {
+                cout << i << " ";
+                i++;
+            }*/
+
+// --------------------------------------------------------------------------
+
+    // Do while loop
+
+            /* int i=12;
+            do{
+                cout<<i<<endl;
+                i++;
+            }
+
+            while(i<10); */
+
+
 }
